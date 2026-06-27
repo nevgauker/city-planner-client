036c7114369992b6811dffe068cb4fc1b7e160b5
rotem <nevgauker@gmail.com>
2026-06-27 18:37:30 +0900

    fix: remove map padding and reposition legend to avoid email overlap
    Map coverage:
    - Removed pt-16 md:pt-4 padding from map container
    - Map now fills entire available space from top to bottom
    - Eliminates white space at top of map
    
    Legend positioning:
    - Mobile: Positioned at top-4 left-4 (top-left of map)
    - Desktop (sm+): Positioned at top-4 right-4 (top-right of map)
    - Moved away from email/QuotaBar which is on the right side
    - No overlap with user email dropdown on any screen size

--- src/components/stages/ItineraryStage.tsx
+++ src/components/stages/ItineraryStage.tsx
@@ -210,7 +210,7 @@ export default function ItineraryStage({ tripData, onBack, existingItinerary }: 
         />
       </div>
 
-      <div className="no-print w-full md:w-2/3 lg:w-3/5 h-1/2 md:h-full relative pt-16 md:pt-4">
+      <div className="no-print w-full md:w-2/3 lg:w-3/5 h-1/2 md:h-full relative">
         <GoogleItineraryMap itinerary={itinerary} selectedActivityIndex={selectedActivityIndex} homeBase={homeBase} onMarkerClick={setSelectedActivityIndex} onHomeBaseClick={() => setSelectedActivityIndex(null)} />
 
         <motion.button
